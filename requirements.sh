75990ff0c280a9f50e2e3498762fd4750b9c58cd
Pedro Dias <jpedrodias@gmail.com>
2026-03-14 14:06:24 +0000

    fix requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 pip3 install proxmoxer
-<<<<<<< HEAD
-=======
-#pip3 install "PySimpleGUI<5.0.0" ---Perdeu suporte, substituimos por tkinter ---
->>>>>>> 75bf57265bc5109f983558c6f76015906959c6d1
 pip3 install requests
